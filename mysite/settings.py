53b127200553333b7349445b3cf50144f953cdcb
cprend2 <54860907+cprend2@users.noreply.github.com>
2019-11-04 15:40:12 -0600

    adding requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku # <- this line goes near the top of the file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,5 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+# Activate Django-Heroku.
+django_heroku.settings(locals()) #
 STATIC_URL = '/static/'
